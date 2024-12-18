7195c182aa057a98309d46146c0f7ced719fa4e9
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2024-12-18 11:14:38 +0530

    init

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "utils/helper.cpp"
+
+
+int32_t main() {
+    std::cout << "Hello SM-DB" << std::endl;
+    print_page_size();
+    return 0;
+}
---
+++ utils/helper.cpp
@@ -0,0 +1,4 @@
+
+void print_page_size() {
+    std::cout << "Page Size: 4096" << std::endl;
+}
